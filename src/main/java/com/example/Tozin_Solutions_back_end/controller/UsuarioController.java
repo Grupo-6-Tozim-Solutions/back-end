715fbce9790f22925900aba946978d569066e5e6
kaiquetozim <kaique.tozim@sptech.school>
2025-04-26 14:44:56 -0300

    implementando CORS

--- src/main/java/com/example/Tozin_Solutions_back_end/controller/UsuarioController.java
+++ src/main/java/com/example/Tozin_Solutions_back_end/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
