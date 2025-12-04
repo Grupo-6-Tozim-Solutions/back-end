8688c6dfcfa33a1d4368c128f3571b3966f0b9ca
Henrique <126406716+pmentta@users.noreply.github.com>
2025-12-04 20:36:23 -0300

    fix

--- src/test/java/com/example/Tozin_Solutions_back_end/TozinSolutionsBackEndApplicationTests.java
+++ src/test/java/com/example/Tozin_Solutions_back_end/TozinSolutionsBackEndApplicationTests.java
@@ -2,8 +2,13 @@ package com.example.Tozin_Solutions_back_end;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import com.example.Tozin_Solutions_back_end.TestConfig;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestConfig.class)
 class TozinSolutionsBackEndApplicationTests {
 
 	@Test
